6000a28f85ed9a5bcefab6cd19080a1b5a8bcb0a
Samy MOSA <samymosa02@gmail.com>
2024-04-20 11:29:33 +0200

    fix: ExpressionChangedAfterItHasBeenCheckedError

--- frontend/src/app/account/components/account-link/account-link.component.ts
+++ frontend/src/app/account/components/account-link/account-link.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { AccountState } from '../../account.state';
@@ -10,4 +10,16 @@ import { AccountState } from '../../account.state';
 export class AccountLinkComponent {
   @Select(AccountState.isAuthenticated)
   declare isAuthenticated$: Observable<boolean>;
+  declare dataTip: string;
+  declare routerLink: string;
+
+  constructor(private cdRef: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.isAuthenticated$.subscribe((isAuthenticated) => {
+      this.dataTip = isAuthenticated ? 'Mon compte' : 'Se connecter';
+      this.routerLink = isAuthenticated ? '/account' : '/account/login';
+      this.cdRef.detectChanges();
+    });
+  }
 }
