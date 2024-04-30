18e68385649dcecc38f9af1611f6da18c431eeb1
Samy MOSA <samymosa02@gmail.com>
2024-04-30 11:26:08 +0200

    Add about me page

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -5,10 +5,12 @@ import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CoreRoutingModule } from './core-routing.module';
+import { AboutMePageComponent } from './pages/about-me-page/about-me-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
+    AboutMePageComponent,
     HomePageComponent,
     NavbarComponent,
     FooterComponent,
