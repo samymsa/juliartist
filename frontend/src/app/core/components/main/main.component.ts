97f7ef860cf1eb5050beacb45001e5e2cb77306e
Samy MOSA <samymosa02@gmail.com>
2024-05-28 11:13:46 +0200

    refactor(core): ChangeDetectionStrategy.OnPush everywhere

--- frontend/src/app/core/components/main/main.component.ts
+++ frontend/src/app/core/components/main/main.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainComponent {}
--- frontend/src/app/core/pages/about-me-page/about-me-page.component.ts
+++ frontend/src/app/core/pages/about-me-page/about-me-page.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-me-page',
   templateUrl: './about-me-page.component.html',
   styleUrl: './about-me-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutMePageComponent {}
